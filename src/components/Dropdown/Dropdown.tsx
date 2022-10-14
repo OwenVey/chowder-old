880bf9d16f52be9ba7e97908fc547db4abe535c9
Owen Vey <owen_vey@vfc.com>
2022-10-14 15:25:55 -0500

    adjust dropdown animations again

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -21,13 +21,17 @@ export default function Dropdown({ align = 'end', trigger, children }: Props) {
             <DropdownMenu.Content asChild align={align} sideOffset={5}>
               <motion.div
                 className="origin-[var(--radix-dropdown-menu-content-transform-origin)] rounded-md border border-gray-7 bg-gray-1 shadow-lg"
-                initial={{ scale: 0, opacity: 0 }}
+                initial={{ scale: 0.9, opacity: 0 }}
                 animate={{
                   scale: 1,
                   opacity: 1,
-                  transition: { type: 'spring', duration: 0.3, bounce: 0 },
+                  transition: { ease: 'easeIn', duration: 0.1, bounce: 0 },
                 }}
-                exit={{ scale: 0, opacity: 0, transition: { duration: 0.1, bounce: 0 } }}
+                exit={{
+                  scale: 0.9,
+                  opacity: 0,
+                  transition: { ease: 'easeOut', duration: 0.075, bounce: 0 },
+                }}
               >
                 {children}
               </motion.div>
