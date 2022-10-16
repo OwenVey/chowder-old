6f5c18b8d2c81ce01e5a96f97c40cdfe4952a41a
Owen Vey <owen_vey@vfc.com>
2022-10-16 16:40:33 -0500

    revert radix plugin back to original way

--- radix-plugin.cjs
+++ radix-plugin.cjs
@@ -2,44 +2,21 @@
 const radix = require('@radix-ui/colors');
 const plugin = require('tailwindcss/plugin');
 
-const createCssVars = (color, radixColors, alpha) => {
-  let colors = {};
-  alpha = alpha || color.slice(-1) === 'A';
-
-  Object.entries(radixColors).forEach(([key, hslValue], index) => {
-    let hslString = /\(([^)]+)\)/.exec(hslValue).pop();
-    if (!alpha) {
-      hslString = hslString.replaceAll(',', '');
-    }
-    colors[`--${color}${alpha ? 'A' : ''}${index + 1}`] = hslString;
-  });
-
-  return colors;
-};
-
 const radixPalette = plugin.withOptions(
   ({ colors = radix } = {}) => {
     let rootColors = {};
     let darkModeColors = {};
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    for (const [key, { color, alpha }] of Object.entries(colors)) {
-      const radixColors = radix[color];
-      const radixColorsDark = radix[`${color}Dark`];
-
-      if (radixColors) {
-        rootColors = { ...rootColors, ...createCssVars(color, radixColors, false) };
-      }
-      if (radixColorsDark) {
-        darkModeColors = { ...darkModeColors, ...createCssVars(color, radixColorsDark, false) };
-      }
-
-      if (alpha) {
-        const radixColorsA = radix[`${color}A`];
-        const radixColorsADark = radix[`${color}DarkA`];
-        rootColors = { ...rootColors, ...createCssVars(color, radixColorsA, true) };
-        darkModeColors = { ...darkModeColors, ...createCssVars(color, radixColorsADark, true) };
-      }
+    for (const [colorName, colorObj] of Object.entries(colors)) {
+      const colorMap = colorName.includes('Dark') ? darkModeColors : rootColors;
+      Object.entries(colorObj).forEach(([, value], index) => {
+        const colorNameWithoutDark = colorName.replace('Dark', '');
+        let hsl = /\(([^)]+)\)/.exec(value).pop();
+        if (colorNameWithoutDark.slice(-1) !== 'A') {
+          hsl = hsl.replaceAll(',', '');
+        }
+        colorMap[`--${colorNameWithoutDark}${index + 1}`] = hsl;
+      });
     }
 
     return ({ addBase }) => {
@@ -52,19 +29,19 @@ const radixPalette = plugin.withOptions(
   ({ colors = radix } = {}) => {
     const themeColors = {};
 
-    for (const [key, { color, alpha }] of Object.entries(colors)) {
-      const themeColor = {};
-      const themeColorA = {};
-      for (let i = 1; i <= 12; i++) {
-        if (alpha || color.slice(-1) === 'A') {
-          themeColorA[i] = `hsla(var(--${color}A${i}))`;
-        }
-        themeColor[i] = `hsl(var(--${color}${i}) / <alpha-value>)`;
+    for (const [colorName, colorObj] of Object.entries(colors)) {
+      if (!colorName.includes('Dark')) {
+        const themeColor = {};
+        Object.entries(colorObj).forEach(([], index) => {
+          if (colorName.slice(-1) === 'A') {
+            themeColor[index + 1] = `hsla(var(--${colorName}${index + 1}))`;
+          } else {
+            themeColor[index + 1] = `hsl(var(--${colorName}${index + 1}) / <alpha-value>)`;
+          }
+        });
+
+        themeColors[colorName] = themeColor;
       }
-      if (alpha) {
-        themeColors[`${key}${alpha ? 'A' : ''}`] = themeColorA;
-      }
-      themeColors[key] = themeColor;
     }
 
     return {
