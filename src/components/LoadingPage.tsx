a37194d3e783e939516abdb44887d6ea6a08b234
Owen Vey <owen_vey@vfc.com>
2022-10-17 16:28:36 -0500

    center LoadingPage vertically

--- src/components/LoadingPage.tsx
+++ src/components/LoadingPage.tsx
@@ -3,7 +3,7 @@ import Logo from './Logo';
 
 export default function LoadingPage() {
   return (
-    <div className="flex w-full items-center justify-center">
+    <div className="flex h-full w-full items-center justify-center">
       <div className="flex flex-col items-center space-y-4">
         <div className="flex items-center">
           <Logo className="h-10 w-auto text-primary-9" />
