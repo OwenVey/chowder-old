1cba99fe0a90c7ee83935220bb56034b4198dbfa
Owen Vey <owen_vey@vfc.com>
2022-10-17 16:28:50 -0500

    clean up default recipe page

--- src/pages/recipes/index.tsx
+++ src/pages/recipes/index.tsx
@@ -2,7 +2,12 @@ import { Button, Dropdown, DropdownItem } from '@/components';
 import RecipeCard from '@/components/RecipeCard';
 import { recipes } from '@/utils/mocks';
 import { ChevronDownIcon, GlobeAltIcon, PencilSquareIcon } from '@heroicons/react/20/solid';
-import { FunnelIcon, MagnifyingGlassIcon, PlusIcon } from '@heroicons/react/24/outline';
+import {
+  DocumentPlusIcon,
+  FunnelIcon,
+  MagnifyingGlassIcon,
+  PlusIcon,
+} from '@heroicons/react/24/outline';
 import { useRouter } from 'next/router';
 import React from 'react';
 
@@ -61,32 +66,16 @@ function RecipesLayout({ children }: Props) {
 
 export default function DefaultRecipePage() {
   return (
-    <div className="text-center">
-      <svg
-        className="mx-auto h-12 w-12 text-gray-400"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        aria-hidden="true"
-      >
-        <path
-          vectorEffect="non-scaling-stroke"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          d="M9 13h6m-3-3v6m-9 1V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z"
-        />
-      </svg>
-      <h3 className="mt-2 text-sm font-medium text-gray-900">No projects</h3>
-      <p className="mt-1 text-sm text-gray-500">Get started by creating a new project.</p>
+    <div className="flex h-full flex-col items-center justify-center px-6 text-center">
+      <div className="mx-auto flex rounded-full bg-gray-3 p-3">
+        <DocumentPlusIcon className="h-8 w-8 text-gray-11" />
+      </div>
+      <h3 className="mt-3 text-sm font-medium text-gray-12">No recipe selected</h3>
+      <p className="mt-1 text-sm text-gray-11">
+        Select a recipe to the left or click the button below to create a new one.
+      </p>
       <div className="mt-6">
-        <button
-          type="button"
-          className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-        >
-          <PlusIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
-          New Project
-        </button>
+        <Button leftIcon={<PlusIcon />}>New Recipe</Button>
       </div>
     </div>
   );
