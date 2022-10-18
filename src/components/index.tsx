9cc594874fc166f94385a444b3efeed09719bff2
Owen Vey <owen_vey@vfc.com>
2022-10-18 18:53:50 -0500

    add Popover component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -8,6 +8,7 @@ export { default as LoadingPage } from './LoadingPage';
 export { default as Logo } from './Logo';
 export { default as MobileHeader } from './MobileHeader';
 export { default as MobileSidebar } from './MobileSidebar';
+export { default as Popover } from './Popover';
 export { default as Search } from './Search';
 export { default as Sidebar } from './Sidebar';
 export { default as ThemeToggler } from './ThemeToggler';
