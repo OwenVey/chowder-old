725e52cd3f81c5e38621de42d1481e2aef66e639
Owen Vey <owen_vey@vfc.com>
2022-10-20 12:04:38 -0500

    add focus rings to buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,7 +6,7 @@ import clsx from 'clsx';
 import React from 'react';
 
 const buttonClasses = cva(
-  'relative inline-flex select-none items-center justify-center rounded-lg border text-sm font-medium transition-colors',
+  'relative inline-flex select-none items-center justify-center rounded-lg border text-sm font-medium transition-colors focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-offset-gray-1',
   {
     variants: {
       disabled: {
@@ -18,7 +18,7 @@ const buttonClasses = cva(
       variant: {
         filled: 'shadow-sm border-transparent',
         default:
-          'border-gray-7 bg-white text-gray-12 shadow-sm hover:border-gray-8 hover:bg-gray-2 active:bg-gray-3 dark:bg-gray-4 dark:hover:bg-gray-5 dark:active:bg-gray-6',
+          'border-gray-7 bg-white text-gray-12 shadow-sm hover:border-gray-8 hover:bg-gray-2 active:bg-gray-3 dark:bg-gray-4 dark:hover:bg-gray-5 dark:active:bg-gray-6 focus-visible:ring-primary-9',
         light: 'border-transparent shadow-sm',
         subtle: 'border-transparent',
       },
@@ -35,93 +35,104 @@ const buttonClasses = cva(
       {
         variant: 'filled',
         color: 'primary',
-        class: 'bg-primary-9 text-white hover:bg-primary-10 active:bg-primary-11',
+        class:
+          'bg-primary-9 text-white hover:bg-primary-10 active:bg-primary-11 focus-visible:ring-primary-9',
       },
       {
         variant: 'filled',
         color: 'gray',
-        class: 'bg-gray-9 text-white hover:bg-gray-10 active:bg-gray-11',
+        class: 'bg-gray-9 text-white hover:bg-gray-10 active:bg-gray-11 focus-visible:ring-gray-9',
       },
       {
         variant: 'filled',
         color: 'red',
-        class: 'bg-red-9 text-white hover:bg-red-10 active:bg-red-11',
+        class: 'bg-red-9 text-white hover:bg-red-10 active:bg-red-11 focus-visible:ring-red-9',
       },
       {
         variant: 'filled',
         color: 'green',
-        class: 'bg-green-9 text-white hover:bg-green-10 active:bg-green-11',
+        class:
+          'bg-green-9 text-white hover:bg-green-10 active:bg-green-11 focus-visible:ring-green-9',
       },
       {
         variant: 'filled',
         color: 'blue',
-        class: 'bg-blue-9 text-white hover:bg-blue-10 active:bg-blue-11',
+        class: 'bg-blue-9 text-white hover:bg-blue-10 active:bg-blue-11 focus-visible:ring-blue-9',
       },
       {
         variant: 'filled',
         color: 'yellow',
-        class: 'bg-yellow-9 text-black hover:bg-yellow-10 active:bg-yellow-11',
+        class:
+          'bg-yellow-9 text-black hover:bg-yellow-10 active:bg-yellow-11 focus-visible:ring-yellow-9',
       },
       {
         variant: 'light',
         color: 'primary',
-        class: 'bg-primary-4 text-primary-11 hover:bg-primary-5 active:bg-primary-6',
+        class:
+          'bg-primary-4 text-primary-11 hover:bg-primary-5 active:bg-primary-6 focus-visible:ring-primary-9',
       },
       {
         variant: 'light',
         color: 'gray',
-        class: 'bg-gray-4 text-gray-12 hover:bg-gray-5 active:bg-gray-6',
+        class: 'bg-gray-4 text-gray-12 hover:bg-gray-5 active:bg-gray-6 focus-visible:ring-gray-9',
       },
       {
         variant: 'light',
         color: 'red',
-        class: 'bg-red-4 text-red-11 hover:bg-red-5 active:bg-red-6',
+        class: 'bg-red-4 text-red-11 hover:bg-red-5 active:bg-red-6 focus-visible:ring-red-9',
       },
       {
         variant: 'light',
         color: 'green',
-        class: 'bg-green-4 text-green-11 hover:bg-green-5 active:bg-green-6',
+        class:
+          'bg-green-4 text-green-11 hover:bg-green-5 active:bg-green-6 focus-visible:ring-green-9',
       },
       {
         variant: 'light',
         color: 'blue',
-        class: 'bg-blue-4 text-blue-11 hover:bg-blue-5 active:bg-blue-6',
+        class: 'bg-blue-4 text-blue-11 hover:bg-blue-5 active:bg-blue-6 focus-visible:ring-blue-9',
       },
       {
         variant: 'light',
         color: 'yellow',
-        class: 'bg-yellow-4 text-yellow-11 hover:bg-yellow-5 active:bg-yellow-6',
+        class:
+          'bg-yellow-4 text-yellow-11 hover:bg-yellow-5 active:bg-yellow-6 focus-visible:ring-yellow-9',
       },
       {
         variant: 'subtle',
         color: 'primary',
-        class: 'text-primary-9 hover:bg-primary-4 hover:text-primary-11 active:bg-primary-5',
+        class:
+          'text-primary-9 hover:bg-primary-4 hover:text-primary-11 active:bg-primary-5 focus-visible:ring-primary-9',
       },
       {
         variant: 'subtle',
         color: 'gray',
-        class: 'text-gray-10 hover:bg-gray-4 hover:text-gray-11 active:bg-gray-5',
+        class:
+          'text-gray-10 hover:bg-gray-4 hover:text-gray-11 active:bg-gray-5 focus-visible:ring-gray-9',
       },
       {
         variant: 'subtle',
         color: 'red',
-        class: 'text-red-9 hover:bg-red-4 hover:text-red-11 active:bg-red-5',
+        class:
+          'text-red-9 hover:bg-red-4 hover:text-red-11 active:bg-red-5 focus-visible:ring-red-9',
       },
       {
         variant: 'subtle',
         color: 'green',
-        class: 'text-green-9 hover:bg-green-4 hover:text-green-11 active:bg-green-5',
+        class:
+          'text-green-9 hover:bg-green-4 hover:text-green-11 active:bg-green-5 focus-visible:ring-green-9',
       },
       {
         variant: 'subtle',
         color: 'blue',
-        class: 'text-blue-9 hover:bg-blue-4 hover:text-blue-11 active:bg-blue-5',
+        class:
+          'text-blue-9 hover:bg-blue-4 hover:text-blue-11 active:bg-blue-5 focus-visible:ring-blue-9',
       },
       {
         variant: 'subtle',
         color: 'yellow',
         class:
-          'text-yellow-10 dark:text-yellow-9 hover:bg-yellow-4 hover:text-yellow-11 active:bg-yellow-5',
+          'text-yellow-10 dark:text-yellow-9 hover:bg-yellow-4 hover:text-yellow-11 active:bg-yellow-5 focus-visible:ring-yellow-9',
       },
     ],
     defaultVariants: {
